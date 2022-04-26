68873ac3408b4eabb056220df2e021ee0eed609a
BrianCS-dev <briancarmona@live.com>
2022-04-26 18:13:16 -0500

    commit

--- UDPClient.java
+++ UDPClient.java
@@ -40,7 +40,7 @@ public class UDPClient {
 
     //turn received name from server to display to the user to see if it is correct
     String modifiedSentence = new String(receivePacket.getData());
-
+    modifiedSentence = modifiedSentence.trim();
     System.out.println("Username according to the server:" + modifiedSentence);
     System.out.println("Hello " + modifiedSentence);
     System.out.println("For this server, you can only enter 3 expressions");
@@ -51,7 +51,7 @@ public class UDPClient {
       receiveData = new byte[1024];
       String sentence;
 
-      System.out.println("Enter expression number: ");
+      System.out.println("Enter expression number: " + (i+1));
       sentence = inFromUser.readLine();
 
       //sends math request to the server
@@ -62,13 +62,19 @@ public class UDPClient {
       clientSocket.receive(receieveRequest);
 
       String mathAnswer = new String(receieveRequest.getData());
+      mathAnswer = mathAnswer.trim();
       System.out.println("The answer to your expression from the server: " + mathAnswer);
 
       //wait a few seconds before sending a new message
       Thread.sleep((1 + r.nextInt(6)) * 1000);
 
     }
-
+    //create a new message to send to the server
+    byte[] byteMessage = new byte[1024];
+    //create the new message sending a close connection flag to the server
+    byteMessage = (modifiedSentence + ":" + "close").trim().getBytes();
+    //send the message as a new packet
+    clientSocket.send(new DatagramPacket(byteMessage, byteMessage.length, IPAddress, 9876));
     clientSocket.close();
   }
 }
